677bcb1c73751dcdce928fe0ec05a7e72265f071
Manasa Putsala <manumanasap1@gmail.com>
2018-06-11 22:26:36 -0700

    Python solved problems after modifications

--- for_loop.py
+++ for_loop.py
@@ -34,6 +34,11 @@ def find_element(l, a):
 
 
 def find_min_idx_element(l):
+    """
+    Find index to the smallest value in the list
+    """
+    if l is None or len(l) == 0:
+        return -1
     min_val = l[0]
     min_index = 0
     cur_idx = 0
@@ -44,20 +49,6 @@ def find_min_idx_element(l):
         cur_idx = cur_idx + 1
 
     return min_index
-
-
-def find_small_element(l):
-    """
-    Returns the smallest element
-    """
-    return
-
-
-def sort_list(l):
-    """
-    Sort integers in list
-    """
-    return l
 
 
 def reverse_list(l):
@@ -85,21 +76,6 @@ def concat_lists(l1, l2):
     for x in l2:
         new_list.append(x)
     return new_list
-
-
-def merge_lists(l1, l2):
-    """
-    l1 and l2 are always sorted and unique elements(no duplicates).
-    Return a new list that merges l1 and l2 in sorted order and remove any duplicates.
-    """
-
-    return []
-
-
-def rotate_left3(l):
-    """
-
-    """
 
 
 def tests():
@@ -130,22 +106,25 @@ def tests():
     assert find_element([3, 3, 3, 3, 4], 3) == 0
     assert find_element([5, 3, 4, '%'], '%') == 3
     assert find_element([1, 2, 'hello'], 'hello') == 2
-    # TODO: Write more test cases
-
     print "All tests for find_element() passed"
 
-    # Tests for sort_list()
-    assert sort_list([3, 5, 1]) == [1, 3, 5]
-    # TODO: Write more test cases
-    print "All tests for sort_list() passed"
+    # Tests for min element index
+    assert find_min_idx_element([1100, 1101, 1103, 1105, -1]) == 4
+    assert find_min_idx_element([52, 42, 0, -4, -100]) == 4
+    assert find_min_idx_element([0, 0, 0, 0]) == 0
+    assert find_min_idx_element([100, -1000, -1000, -1]) == 1
+    assert find_min_idx_element([]) == -1
+    assert find_min_idx_element([1, 100000000000, -10000000000000000000000]) == 2
+    assert find_min_idx_element(['h', 'i', 'a', 'A']) == 3
+    assert find_min_idx_element(['hello', 'Hello']) == 1
+    assert find_min_idx_element(['Hello', '123']) == 1
+    print "All tests for find_min_idx_element() passed"
 
     # Tests for reverse_list()
     assert reverse_list([3, 5, 1]) == [1, 5, 3]
     assert reverse_list([]) == []
     assert reverse_list(['@', '&', '%']) == ['%', '&', '@']
     assert reverse_list(['hello']) == ['hello']
-    # assert reverse_list([],[]]) == []
-    # TODO: Write more test cases
     print "All tests for reverse_list() passed"
 
     # Tests for concat_lists()
@@ -156,25 +135,9 @@ def tests():
     assert concat_lists([], []) == []
     assert concat_lists(['s', 'app', 'hello', 3], [2, 3]) == ['s', 'app', 'hello', 3, 2, 3]
     assert concat_lists(['#', '$', '&'], ['!', '*']) == ['#', '$', '&', '!', '*']
-    # assert concat_lists([]) == []
-    # TODO: Write more test cases
     print "All tests for concat_lists() passed"
 
     return
-
-    # Tests for merge_lists()
-    assert merge_lists([1, 3, 5], [4, 8]) == [1, 3, 4, 5, 8]
-    # TODO: Write more test cases
-    print "All tests for merge_lists() passed"
-
-    # Tests for min element index
-    assert find_min_idx_element([1100, 1101, 1103, 1105, -1]) == -1
-    print "All tests for find_min_idx_element() passed"
-
-    # Tests for rotate_left3
-    assert rotate_left3([1, 2, 3]) == [2, 3, 1]
-    assert rotate_left3([5, 11, 9]) == [11, 9, 5]
-    assert rotate_left3([7, 0, 0]) == [0, 0, 7]
 
 
 if __name__ == "__main__":
